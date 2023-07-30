5dbe93259b2887fdb0bba15f0f9d75eb67257f24
Crispin <crispinr003@gmail.com>
2023-07-30 13:48:27 +0530

    refactor: clear clutter, add elevenlabs sdk
    Co-authored-by: Alwin Jebastine <alwinjebastine@users.noreply.github.com>
    Co-authored-by: Regis Raymond <RegisRay@users.noreply.github.com>

--- app/api/tts/route.ts
+++ app/api/tts/route.ts
@@ -1,34 +1,19 @@
-import axios from "axios"
 import { NextRequest, NextResponse } from "next/server"
+const voice = require("elevenlabs-node")
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  try {
-    const { voiceId } = await req.json()
+  const { voice_id } = await req.json()
 
-    const data = await axios.post(
-      `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`,
-      {
-        text: "h",
-        model_id: "eleven_monolingual_v1", // or you can change to monolingual v4
-        voice_settings: {
-          stability: 0.5,
-          similarity_boost: 0.5,
-        },
-      },
-      {
-        headers: {
-          accept: "audio/mpeg",
-          "xi-api-key": process.env.ELEVENLABS_API_KEY as string,
-          "Content-Type": "audio/mpeg",
-        },
-      }
-    )
-    if (data) {
-      console.log(data)
+  const fileName = "audio.mp3" // The name of your audio file
+  const textInput = "hello" // The text you wish to convert to speech
 
-      return NextResponse.json(data)
-    }
-  } catch (e) {
-    return NextResponse.json({ error: e.message }, { status: 500 })
-  }
+  voice
+    .textToSpeech(process.env.ELEVENLABS_API_KEY, voice_id, fileName, textInput)
+    .then((resp) => {
+      console.log(resp)
+      return NextResponse.json({ nice: resp.status }, { status: 200 })
+    })
+    .catch((e) => {
+      return NextResponse.json({ error: e.message }, { status: 500 })
+    })
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,14 @@
 "use client"
-
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/tts")
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data")
-  }
-
-  return res.json()
-}
+import { useEffect } from "react"
 
 export default async function Page() {
-  const blob = await getData()
-
   return (
-    <main>
-      <div>
-        <h2>Audio player using binary data</h2>
-      </div>
-    </main>
+    <>
+      <main>
+        <div>
+          <h2>Audio player using binary data</h2>
+        </div>
+      </main>
+    </>
   )
 }
